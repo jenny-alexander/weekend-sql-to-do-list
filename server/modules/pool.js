fbb55ff76ff599d56fdac2ceb664a9bb3bf40946
Jennifer Alexander <jenny_alexander@icloud.com>
2021-10-20 16:24:32 -0500

    Try to use Heroku db

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,31 @@
 //require pg first
 const pg = require('pg');
 
-const pool = new pg.Pool({
-    database: 'weekend_to_do_app',
-    host: 'localhost',
-    port: 5432,
-    max: 12,
-    idleTimeoutMillis: 30000
+// const pool = new pg.Pool({
+//     database: 'weekend_to_do_app',
+//     host: 'localhost',
+//     port: 5432,
+//     max: 12,
+//     idleTimeoutMillis: 30000
+// });
+
+// NEW START
+let pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+if ( pool.connectionString == undefined ) {
+    pool = new pg.Pool({
+            database: 'weekend_to_do_app',
+            host: 'localhost',
+            port: 5432,
+            max: 12,
+            idleTimeoutMillis: 30000
+        });    
+}
+//NEW END
 
 //export
 module.exports = pool;
